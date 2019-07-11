6c87aafd5732f7724aec8294959d828204031aa1
Amaury <amaury.tobiasqr@gmail.com>
2019-07-11 06:13:10 -0500

    Implementacion inicial del componente

--- dist/v-avatar.ssr.js
+++ dist/v-avatar.ssr.js
@@ -1,11 +1,193 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});var script = {
-    name: 'VAvatar', // vue component name
-    data: function data() {
-        return {
-            counter: 5,
-            initCounter: 5,
-        };
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});//
+//
+//
+//
+//
+//
+
+var script = {
+  name: "VAvatar",
+  props: {
+    username: {
+      type: String
     },
+    initials: {
+      type: String
+    },
+    backgroundColor: {
+      type: String
+    },
+    color: {
+      type: String
+    },
+    customStyle: {
+      type: Object
+    },
+    inline: {
+      type: Boolean
+    },
+    size: {
+      type: Number,
+      default: 50
+    },
+    src: {
+      type: String
+    },
+    rounded: {
+      type: Boolean,
+      default: true
+    },
+    lighten: {
+      type: Number,
+      default: 80
+    }
+  },
+
+  data: function data() {
+    return {
+      backgroundColors: [
+        "#F44336",
+        "#FF4081",
+        "#9C27B0",
+        "#673AB7",
+        "#3F51B5",
+        "#2196F3",
+        "#03A9F4",
+        "#00BCD4",
+        "#009688",
+        "#4CAF50",
+        "#8BC34A",
+        "#CDDC39",
+        /* '#FFEB3B' , */ "#FFC107",
+        "#FF9800",
+        "#FF5722",
+        "#795548",
+        "#9E9E9E",
+        "#607D8B"
+      ]
+    };
+  },
+
+  mounted: function mounted() {
+    if (!this.isImage) {
+      this.$emit("avatar-initials", this.username, this.userInitial);
+    }
+  },
+
+  computed: {
+    background: function background() {
+      if (!this.isImage) {
+        return (
+          this.backgroundColor ||
+          this.randomBackgroundColor(
+            this.username.length,
+            this.backgroundColors
+          )
+        );
+      }
+    },
+
+    fontColor: function fontColor() {
+      if (!this.isImage) {
+        return this.color || this.lightenColor(this.background, this.lighten);
+      }
+    },
+
+    isImage: function isImage() {
+      return Boolean(this.src);
+    },
+
+    style: function style() {
+      var style = {
+        display: this.inline ? "inline-flex" : "flex",
+        width: ((this.size) + "px"),
+        height: ((this.size) + "px"),
+        borderRadius: this.rounded ? "50%" : 0,
+        lineHeight: ((this.size + Math.floor(this.size / 20)) + "px"),
+        fontWeight: "bold",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center"
+      };
+
+      var imgBackgroundAndFontStyle = {
+        background: ("transparent url('" + (this.src) + "') no-repeat scroll 0% 0% / " + (this.size) + "px " + (this.size) + "px content-box border-box")
+      };
+
+      var initialBackgroundAndFontStyle = {
+        backgroundColor: this.background,
+        font: ((Math.floor(this.size / 2.5)) + "px/" + (this.size) + "px Helvetica, Arial, sans-serif"),
+        color: this.fontColor
+      };
+
+      var backgroundAndFontStyle = this.isImage
+        ? imgBackgroundAndFontStyle
+        : initialBackgroundAndFontStyle;
+
+      Object.assign(style, backgroundAndFontStyle);
+
+      return style;
+    },
+
+    userInitial: function userInitial() {
+      if (!this.isImage) {
+        var initials = this.initials || this.initial(this.username);
+        return initials;
+      }
+      return "";
+    }
+  },
+
+  methods: {
+    initial: function initial(username) {
+      var parts = username.split(/[ -]/);
+      var initials = "";
+
+      for (var i = 0; i < parts.length; i++) {
+        initials += parts[i].charAt(0);
+      }
+
+      if (initials.length > 3 && initials.search(/[A-Z]/) !== -1) {
+        initials = initials.replace(/[a-z]+/g, "");
+      }
+
+      initials = initials.substr(0, 3).toUpperCase();
+
+      return initials;
+    },
+
+    randomBackgroundColor: function randomBackgroundColor(seed, colors) {
+      return colors[seed % colors.length];
+    },
+
+    lightenColor: function lightenColor(hex, amt) {
+      // From https://css-tricks.com/snippets/javascript/lighten-darken-color/
+      var usePound = false;
+
+      if (hex[0] === "#") {
+        hex = hex.slice(1);
+        usePound = true;
+      }
+
+      var num = parseInt(hex, 16);
+      var r = (num >> 16) + amt;
+
+      if (r > 255) { r = 255; }
+      else if (r < 0) { r = 0; }
+
+      var b = ((num >> 8) & 0x00ff) + amt;
+
+      if (b > 255) { b = 255; }
+      else if (b < 0) { b = 0; }
+
+      var g = (num & 0x0000ff) + amt;
+
+      if (g > 255) { g = 255; }
+      else if (g < 0) { g = 0; }
+
+      return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+    }
+  }
 };function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
 /* server only */
 , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
@@ -89,75 +271,25 @@
   return script;
 }
 
-var normalizeComponent_1 = normalizeComponent;function createInjectorSSR(context) {
-  if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-    context = __VUE_SSR_CONTEXT__;
-  }
-
-  if (!context) { return function () {}; }
-
-  if (!('styles' in context)) {
-    context._styles = context._styles || {};
-    Object.defineProperty(context, 'styles', {
-      enumerable: true,
-      get: function get() {
-        return context._renderStyles(context._styles);
-      }
-    });
-    context._renderStyles = context._renderStyles || renderStyles;
-  }
-
-  return function (id, style) {
-    return addStyle(id, style, context);
-  };
-}
-
-function addStyle(id, css, context) {
-  var group =  css.media || 'default' ;
-  var style = context._styles[group] || (context._styles[group] = {
-    ids: [],
-    css: ''
-  });
-
-  if (!style.ids.includes(id)) {
-    style.media = css.media;
-    style.ids.push(id);
-    var code = css.source;
-
-    style.css += code + '\n';
-  }
-}
-
-function renderStyles(styles) {
-  var css = '';
-
-  for (var key in styles) {
-    var style = styles[key];
-    css += '<style data-vue-ssr-id="' + Array.from(style.ids).join(' ') + '"' + (style.media ? ' media="' + style.media + '"' : '') + '>' + style.css + '</style>';
-  }
-
-  return css;
-}
-
-var server = createInjectorSSR;/* script */
+var normalizeComponent_1 = normalizeComponent;/* script */
 var __vue_script__ = script;
 
 /* template */
-var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"v-avatar"},[_vm._ssrNode("<p>The counter is set to <b>"+_vm._ssrEscape(_vm._s(_vm.counter))+"</b>.</p> <button>Click +1</button> <button>Click -1</button> <button>Reset</button>")])};
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"vue-avatar--wrapper",style:([_vm.style, _vm.customStyle]),attrs:{"aria-hidden":"true"}},[_vm._ssrNode("<span"+(_vm._ssrStyle(null,null, { display: (!this.isImage) ? '' : 'none' }))+">"+_vm._ssrEscape(_vm._s(_vm.userInitial))+"</span>")])};
 var __vue_staticRenderFns__ = [];
 
   /* style */
-  var __vue_inject_styles__ = function (inject) {
-    if (!inject) { return }
-    inject("data-v-2e143e16_0", { source: ".v-avatar[data-v-2e143e16]{display:block;width:400px;margin:25px auto;border:1px solid #ccc;background:#eaeaea;text-align:center;padding:25px}.v-avatar p[data-v-2e143e16]{margin:0 0 1em}", map: undefined, media: undefined });
-
-  };
+  var __vue_inject_styles__ = undefined;
   /* scoped */
-  var __vue_scope_id__ = "data-v-2e143e16";
+  var __vue_scope_id__ = undefined;
   /* module identifier */
-  var __vue_module_identifier__ = "data-v-2e143e16";
+  var __vue_module_identifier__ = "data-v-369cfb7c";
   /* functional template */
   var __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
 
   
   var component = normalizeComponent_1(
@@ -168,7 +300,7 @@ var __vue_staticRenderFns__ = [];
     __vue_is_functional_template__,
     __vue_module_identifier__,
     undefined,
-    server
+    undefined
   );// Import vue component
 
 // install function executed by Vue.use()
